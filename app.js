7c40c43520f77bc6664661badbe186a80a7f4c19
joblackpoc <duopoc@gmail.com>
2017-08-10 23:08:45 +0700

    another change

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
+const path = require('path');
+// Init App
 const app = express();
 
-app.get('/',function(req, res){
+//Load View Engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'pug');
+
+// Home Route
+/*app.get('/',function(req, res){
   res.send('Hello World');
 });
-
+*/
+app.get('/',function(req, res){
+    res.render('index',{
+      title:'Hello world World'
+    });
+});
+// Start Server
 app.listen(3000, function(){
-  console.log('Server start on port 3000');
+  console.log('Server start on port 3000...');
 });
