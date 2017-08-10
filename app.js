1d6dc28580262de95dc8f2625d5f94550fdda545
joblackpoc <duopoc@gmail.com>
2017-08-10 21:55:13 +0700

    another change

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const express = require('express');
+
+const app = express();
+
+app.get('/',function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(3000, function(){
+  console.log('Server start on port 3000');
+});
